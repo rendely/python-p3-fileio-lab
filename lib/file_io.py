0bcf285fc4cd5a25b9226a5ec934d09cf069b818
matthew rendely <matthew.rendely@gmail.com>
2023-06-18 11:26:54 -0700

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,15 @@
 def write_file(file_name, file_content):
-    pass
+    with open(f'{file_name}.txt', mode='w', encoding='utf-8') as file:
+        file.write(file_content)
 
 def append_file(file_name, append_content):
-    pass
+     with open(f'{file_name}.txt', mode='a', encoding='utf-8') as file:
+        file.write(append_content)
 
 def read_file(file_name):
-    pass
+    with open(f'{file_name}.txt', encoding='utf-8') as file:
+        return file.read()
+
+# write_file('matt_test.txt', 'hello')
+# append_file('matt_test.txt', 'appending this')
+# read_file('matt_test.txt')
